38aa1e7ef9d9383221f2ad17fb2b00776798d9e7
Arpit Thool <arpitthool@vt.edu>
2025-09-19 22:08:20 -0400

    - added demo image to experiment with.
    	new file:   images/objects.png
    
    - added image path as string and showing image till 'q' is pressed to exit.
    
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
+using namespace cv;
 using namespace std;
 
 int main () {
 
-    cout << "Hello World!";
+    string imagePath = "../images/objects.png";
+    Mat image = imread(imagePath);
+
+    if (image.empty()) {
+        cout << "Error: Could not load image from " << imagePath << endl;
+        return -1;
+    }
+
+    while(true){
+        imshow("Cutout tool", image);
+
+        char key = waitKey(20);
+
+        if(key == 'q'){
+            cout << "Goodbye!";
+            break;
+        }
+
+    }
+
+    return 0;
 }
