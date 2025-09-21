037282090745b1501bc6ce684f1f857a96dc3eab
Arpit Thool <arpitthool@vt.edu>
2025-09-20 22:29:48 -0400

    - added new file to experiment
    	new file:   images/dog.png
    
    - added option 'g' to automatically generate a cutout of the object selected in rectangle. But still need to refine the mask because right now just the cropped image is returned.
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 bool selectRectangleMode = false, firstClickDone = false;
 Point firstClick, secondClick;      // Variables to store the coordinates of the two clicks
 Mat image;  
+Rect selectedRect;                  // Rectangle selected by user
+Mat bgModel, fgModel, mask;         // Variables for GrabCut segmentation
 
 // Mouse callback function for rectangle selection - this function is called whenever the mouse is clicked
 // Where x and y are the coordinates of the mouse click and event is the type of event that occurred
@@ -38,12 +40,21 @@ void mouseCallback(int event, int x, int y, int flags, void* userdata) {
             // This handles cases where user clicks bottom-right first, then top-left
             Point topLeft = Point(min(firstClick.x, secondClick.x), min(firstClick.y, secondClick.y));
             Point bottomRight = Point(max(firstClick.x, secondClick.x), max(firstClick.y, secondClick.y));
+
+            selectedRect = Rect(topLeft, bottomRight);
             
             // Draw a rectangle on the image with 2-pixel thickness
             rectangle(image, topLeft, bottomRight, Scalar(0, 255, 0), 2);
             
             cout << "Rectangle selected: Top-left (" << topLeft.x << ", " << topLeft.y 
                  << "), Bottom-right (" << bottomRight.x << ", " << bottomRight.y << ")" << endl;
+            
+            // Run GrabCut on the selected rectangle
+            mask = Mat(image.size(), CV_8UC1, GC_BGD);
+            
+            grabCut(image, mask, selectedRect, bgModel, fgModel, 5, GC_INIT_WITH_RECT);
+            
+            cout << "GrabCut segmentation completed! Press 'g' to see the result." << endl;
         }
     }
 }
@@ -85,6 +96,23 @@ int main () {
                 cout << "Rectangle selection mode activated. Click for top-left corner." << endl;
             } else {
                 cout << "Already in rectangle selection mode. Complete the current selection first." << endl;
+            }
+        } else if( key == 'g') {
+            // Check if we have a valid rectangle and mask from GrabCut
+            if(selectedRect.width > 0 && selectedRect.height > 0 && !mask.empty()) {
+                // Create a result image with only the foreground
+                Mat result;
+               
+                // create cutout
+                image.copyTo(result, mask);
+                
+                // Show the cutout result in a new window
+                imshow("Cutout Mask", mask);
+                imshow("GrabCut Result", result);
+
+                cout << "GrabCut result displayed! Close the result window to continue." << endl;
+            } else {
+                cout << "No rectangle selected or GrabCut not run yet. Press 'r' to select a rectangle first." << endl;
             }
         }
 
